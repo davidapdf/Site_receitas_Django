1738f2af1fa831a2774f27acb1bc3c8f05ebeb42
David Alexandre <david.paula.dias@everis.com>
2020-02-28 17:39:31 -0300

    Atualização Admin

--- receitas/receitaap/admin.py
+++ receitas/receitaap/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-
 from .models import Receitas
 
-admin.site.register(Receitas)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id','nome_receita','categoria','data_receite','tempo_de_preparo')
+    list_display_links = ('id','nome_receita')
+    search_fields=('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 10
+
+admin.site.register(Receitas,ListandoReceitas)
