789cea7767ea227be251a1fb36e891af21dd0000
David Alexandre <david.paula.dias@everis.com>
2020-03-10 16:08:57 -0300

    incusão botão excluir e editar receita

--- receitas/usuarios/views.py
+++ receitas/usuarios/views.py
@@ -99,4 +99,23 @@ def deleta_receita(request, receita_id):
     receita = get_object_or_404(Receitas,pk=receita_id)
     receita.delete()
     return redirect('dashboard')
-    pass
+
+def edita_receita(request,receita_id):
+    receita = get_object_or_404(Receitas,pk=receita_id)
+    receita_editar = {'receita':receita}
+    return render(request,'usuarios/edita_receitas.html',receita_editar)
+
+def atualiza_receita(request):
+    if request.method == 'POST':
+        receita_id = request.POST['receita_id']
+        r = Receitas.objects.get(pk=receita_id)
+        r.nome_receita = request.POST['nome_receita']
+        r.ingredientes = request.POST['ingredientes']
+        r.modo_preparo = request.POST['modo_preparo']
+        r.tempo_preparo = request.POST['tempo_preparo']
+        r.rendimento = request.POST['rendimento']
+        r.categoria = request.POST['categoria']
+        if 'foto_receita' in request.FILES:
+            r.foto_receita = request.FILES['foto_receita']
+        r.save()
+        return redirect('dashboard')
